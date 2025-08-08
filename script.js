028a102c3fc0a89eb7c1c8e0ecd619d84f124672
Ahmed Salem <superahmed208@gmail.com>
2025-08-08 12:09:28 -0400

    added rainbow effect toggle, made title wave position stay between pages

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let letterTimers = [7,6,5,4,3,2,1,0]
+// Cookie handling functions from https://www.quirksmode.org/js/cookies.html
+function createCookie(name,value) {
+	document.cookie = name+"="+value+"; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+if(readCookie('doRainbow') == null){
+    createCookie('doRainbow', 'false')
+}
+
+if(readCookie('hue') == null){
+    createCookie('hue', '0')
+}
+
+if(readCookie('letterTimers') == null){
+    createCookie('letterTimers', JSON.stringify([7,6,5,4,3,2,1,0]))
+}
+
+let letterTimers = JSON.parse(readCookie('letterTimers'))
 let letters = [
     document.getElementById("title-header-1"),
     document.getElementById("title-header-2"),
@@ -7,9 +34,7 @@ let letters = [
     document.getElementById("title-header-5"),
     document.getElementById("title-header-6"),
     document.getElementById("title-header-7"),
-    document.getElementById("title-header-8")
-]
-
+    document.getElementById("title-header-8")]
 function waveTitle(){
     const speed = 0.015
     const height = 0.5
@@ -19,16 +44,34 @@ function waveTitle(){
         letterTimers[i] += speed
     }
 
+    createCookie('letterTimers', JSON.stringify(letterTimers))
     requestAnimationFrame(waveTitle)
 }
-
-let hue = 0
-function rainbow(){
-    document.documentElement.style.filter = "hue-rotate("+hue+"deg)"
-    hue += 3
-
-    requestAnimationFrame(rainbow)
-}
-
 waveTitle()
-// rainbow()
+
+let hue = Number(readCookie('hue'))
+let doRainbow = readCookie('doRainbow') === 'true'
+if(doRainbow){rainbow()}
+function rainbow(){
+    const speed = 1.5
+    
+    if(doRainbow){
+        document.documentElement.style.filter = "hue-rotate("+hue+"deg)"
+        hue += speed
+        
+        createCookie('hue', `${hue}`)
+        requestAnimationFrame(rainbow)
+    }else{
+        document.documentElement.style.filter = "hue-rotate(0deg)"
+    }
+}
+function toggleRainbow(){
+    if(doRainbow){
+        createCookie('doRainbow', 'false')
+        doRainbow = false
+    }else{
+        createCookie('doRainbow', 'true')
+        doRainbow = true
+        rainbow()
+    }
+}
