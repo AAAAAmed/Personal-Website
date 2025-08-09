690368273d39b837d449425fb021d74e34016807
Ahmed Salem <superahmed208@gmail.com>
2025-08-08 22:05:04 -0400

    added hue slider

--- header.js
+++ header.js
@@ -1,3 +1,32 @@
+// Cookie handling functions from https://www.quirksmode.org/js/cookies.html
+function createCookie(name,value) {
+	document.cookie = name+"="+value+"; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+// Initialize cookies if they don't exist
+if(readCookie('doRainbow') == null){
+    createCookie('doRainbow', 'false')
+}
+if(readCookie('hue') == null){
+    createCookie('hue', '0')
+}
+if(readCookie('baseHue') == null){
+    createCookie('baseHue', '0')
+}
+if(readCookie('letterTimers') == null){
+    createCookie('letterTimers', JSON.stringify([7,6,5,4,3,2,1,0]))
+}
+
 // Injects header and nav into HTML
 const header = document.createElement('header')
 
@@ -5,6 +34,18 @@ const button = document.createElement('button')
 button.textContent = '🌈'
 button.setAttribute('onclick', 'toggleRainbow()')
 button.id = 'rainbow-toggle'
+
+const slider = document.createElement('input')
+let baseHue = readCookie('baseHue')
+slider.type = 'range'
+slider.min = 0
+slider.max = 360
+slider.value = baseHue
+slider.id = 'color-slider'
+slider.onchange = (event) => {
+    baseHue = event.target.value
+    createCookie('baseHue', `${event.target.value}`)
+}
 
 const div = document.createElement('div')
 div.id = 'title'
@@ -91,32 +132,7 @@ for(i=0;i<8;i++){
 header.append(nav)
 for(i=0;i<5;i++) nav.append(links[i])
 nav.append(button)
-
-// Cookie handling functions from https://www.quirksmode.org/js/cookies.html
-function createCookie(name,value) {
-	document.cookie = name+"="+value+"; path=/";
-}
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-// Initialize cookies if they don't exist
-if(readCookie('doRainbow') == null){
-    createCookie('doRainbow', 'false')
-}
-if(readCookie('hue') == null){
-    createCookie('hue', '0')
-}
-if(readCookie('letterTimers') == null){
-    createCookie('letterTimers', JSON.stringify([7,6,5,4,3,2,1,0]))
-}
+nav.append(slider)
 
 // Handles waving the title around
 let letterTimers = JSON.parse(readCookie('letterTimers'))
@@ -143,7 +159,7 @@ function rainbow(deltaTime){
 
         createCookie('hue', `${hue}`)
     }else{
-        document.documentElement.style.filter = "hue-rotate(0deg)"
+        document.documentElement.style.filter = "hue-rotate("+baseHue+"deg)"
     }
 }
 
