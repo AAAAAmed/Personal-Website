9b879abb7ad7df86d49e1487e6c07a57ccb653dc
Ahmed Salem <superahmed208@gmail.com>
2025-08-08 20:16:15 -0400

    made header modular

---
+++ header.js
@@ -0,0 +1,172 @@
+// Injects header and nav into HTML
+const header = document.createElement('header')
+
+const button = document.createElement('button')
+button.textContent = '🌈'
+button.setAttribute('onclick', 'toggleRainbow()')
+button.id = 'rainbow-toggle'
+
+const div = document.createElement('div')
+div.id = 'title'
+
+const nav = document.createElement('nav')
+const links = []
+for(i=0;i<5;i++){
+    switch(i){
+        case 0:
+            if(window.location.pathname.split('/').pop() == 'index.html'){
+                links.push(document.createElement('span'))
+            }else{
+                links.push(document.createElement('a'))
+                links[i].href = 'index.html'
+            }
+            links[i].textContent = 'Home'
+            break
+
+        case 1:
+            if(window.location.pathname.split('/').pop() == 'about.html'){
+                links.push(document.createElement('span'))
+            }else{
+                links.push(document.createElement('a'))
+                links[i].href = 'about.html'
+            }
+            links[i].textContent = 'About'
+            break
+
+        case 2:
+            if(window.location.pathname.split('/').pop() == 'blog.html'){
+                links.push(document.createElement('span'))
+            }else{
+                links.push(document.createElement('a'))
+                links[i].href = 'blog.html'
+            }
+            links[i].textContent = 'Blog'
+            break
+        
+        case 3:
+            if(window.location.pathname.split('/').pop() == 'projects.html'){
+                links.push(document.createElement('span'))
+            }else{
+                links.push(document.createElement('a'))
+                links[i].href = 'projects.html'
+            }
+            links[i].textContent = 'Projects'
+            break
+
+        case 4:
+            if(window.location.pathname.split('/').pop() == 'links.html'){
+                links.push(document.createElement('span'))
+            }else{
+                links.push(document.createElement('a'))
+                links[i].href = 'links.html'
+            }
+            links[i].textContent = 'Links'
+            break
+    }
+}
+
+const h1s = []
+for(i=0;i<8;i++){
+    h1s.push(document.createElement('h1'))
+    h1s[i].textContent = 'a'
+    h1s[i].id = `th-${i+1}`
+    h1s[i].setAttribute('class', 'title-header')
+}
+h1s[5].textContent = 'm'
+h1s[6].textContent = 'e'
+h1s[7].textContent = 'd'
+
+const spans = []
+for(i=0;i<8;i++){
+    spans.push(document.createElement('span'))
+    spans[i].textContent = ' '
+}
+
+document.body.prepend(header)
+header.append(button)
+header.append(div)
+for(i=0;i<8;i++){
+    div.append(h1s[i])
+    div.append(spans[i])
+}
+header.append(nav)
+for(i=0;i<5;i++) nav.append(links[i])
+
+// Cookie handling functions from https://www.quirksmode.org/js/cookies.html
+function createCookie(name,value) {
+	document.cookie = name+"="+value+"; path=/";
+}
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+// Initialize cookies if they don't exist
+if(readCookie('doRainbow') == null){
+    createCookie('doRainbow', 'false')
+}
+if(readCookie('hue') == null){
+    createCookie('hue', '0')
+}
+if(readCookie('letterTimers') == null){
+    createCookie('letterTimers', JSON.stringify([7,6,5,4,3,2,1,0]))
+}
+
+// Handles waving the title around
+let letterTimers = JSON.parse(readCookie('letterTimers'))
+function waveTitle(deltaTime){
+    const speed = 0.0025
+    const height = 0.25
+
+    for(i=0;i<8;i++){
+        h1s[i].style.top = (Math.sin(letterTimers[i])+1) * height + "rem"
+        letterTimers[i] += speed*deltaTime
+    }
+
+    createCookie('letterTimers', JSON.stringify(letterTimers))
+}
+
+// Handles the togglable rainbow effect
+let hue = Number(readCookie('hue'))
+let doRainbow = readCookie('doRainbow') === 'true'
+function rainbow(deltaTime){
+    const speed = 0.2
+    if(doRainbow){
+        document.documentElement.style.filter = "hue-rotate("+hue+"deg)"
+        hue += speed*deltaTime
+
+        createCookie('hue', `${hue}`)
+    }else{
+        document.documentElement.style.filter = "hue-rotate(0deg)"
+    }
+}
+
+function toggleRainbow(){
+    if(doRainbow){
+        createCookie('doRainbow', 'false')
+        doRainbow = false
+    }else{
+        createCookie('doRainbow', 'true')
+        doRainbow = true
+    }
+}
+
+// Continuously runs other functions and calculates deltaTime
+let lastTime = performance.now()
+function update(){
+    const currentTime = performance.now()
+    const deltaTime = currentTime - lastTime
+    lastTime = currentTime
+    
+    waveTitle(deltaTime)
+    rainbow(deltaTime)
+
+    requestAnimationFrame(update)
+}
+update()
