1de5740f76e74c97aed3abcdf3ebb5241a217b63
Ahmed Salem <superahmed208@gmail.com>
2025-08-24 17:45:39 -0400

    blogs working

--- blog.js
+++ blog.js
@@ -1,12 +1,25 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js"
+
 const blogSection = document.getElementById('blog')
 const url = new URL(window.location.href)
 
 const backButton = document.createElement('a')
 backButton.href = './blog.html'
-backButton.textContent = '<-- Exit blog post'
+backButton.innerHTML = '<b><-- Exit blog post</b>'
 
 if(url.searchParams.has('blog')){
-    blogSection.textContent = 'you are viewing a blog post!'
-    blogSection.prepend(document.createElement('br'))
-    blogSection.prepend(backButton)
+    // This fetch code is mostly written by Gemini
+    fetch(`./blogs/${url.searchParams.get('blog')}.md`)
+        .then(response => {
+            if (!response.ok){
+                throw new Error(`HTTP ERROR: status = ${response.status}`)
+            }
+            return response.text()
+        })
+        .then(textData => {
+            blogSection.innerHTML = marked.parse(textData)
+            blogSection.prepend(document.createElement('br'))
+            blogSection.prepend(backButton)
+        })
+        .catch(error => console.error('Error fetching or reading file:', error))
 }
--- blogs/test.md
+++ blogs/test.md
@@ -1,2 +1,6 @@
-# Test
+## Test
 this is a test blog post
+
+### There is no way that this is just it
+It's way too easy.
+this should be impossible
